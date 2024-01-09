c198c0ebad4f549f10508ad164762c4df3932bff
Kesava Mallela <kmallela@apple.com>
2024-01-09 09:12:51 -0800

    figcaption

--- src/components/card.jsx
+++ src/components/card.jsx
@@ -1,11 +1,13 @@
 export default function Card({character}) {
     return (
         <div className="card w-96 bg-base-100 shadow-xl">
-            <figure className="px-10 pt-10"><img src={character.image} alt={character.iast.sound}/></figure>
+            <figure className="px-10 pt-10">
+                <img src={character.image} alt={character.iast.sound}/>
+                <figcaption className="pronunciation">{character.iast.beginning}<b className="text-xl">{character.iast.sound}</b>{character.iast.end} ⎯ {character.meaning}</figcaption>
+            </figure>
             <div className="card-body items-center text-center">
-                <p className="sound text-4xl italic">{character.iast.sound}</p>
+                <p className="sound text-4xl">{character.iast.sound}</p>
                 <p className="pronunciation text-xl" dangerouslySetInnerHTML={{__html: character.pronunciation}}></p>
-                <p className="pronunciation">{character.iast.beginning}<b className="text-xl">{character.iast.sound}</b>{character.iast.end} ⎯ {character.telugu.beginning}<b>{character.telugu.sound}</b>{character.telugu.end} ⎯ {character.meaning}</p>
 
                 <div className="overflow-x-auto">
                     <table className="table">
