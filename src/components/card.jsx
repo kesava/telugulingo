ed8c920a8ab6ed0f9cdd607ca6cb29fed919b81e
Kesava Mallela <kmallela@apple.com>
2024-01-08 17:09:36 -0800

    added pronunciation

--- src/components/card.jsx
+++ src/components/card.jsx
@@ -1,12 +1,11 @@
 export default function Card({character}) {
     return (
         <div className="card w-96 bg-base-100 shadow-xl">
-            <figure><img src={character.image} alt={character.iast.sound}/></figure>
+            <figure className="px-10 pt-10"><img src={character.image} alt={character.iast.sound}/></figure>
             <div className="card-body items-center text-center">
                 <p className="sound text-4xl italic">{character.iast.sound}</p>
-                <p className="pronunciation">{character.iast.beginning}<b className="text-xl">{character.iast.sound}</b>{character.iast.end}</p>
-                <p className="pronunciation" dangerouslySetInnerHTML={{__html: character.pronunciation}}></p>
-                <p>{character.telugu.beginning}<b>{character.telugu.sound}</b>{character.telugu.end} -- {character.meaning}</p>
+                <p className="pronunciation text-xl" dangerouslySetInnerHTML={{__html: character.pronunciation}}></p>
+                <p className="pronunciation">{character.iast.beginning}<b className="text-xl">{character.iast.sound}</b>{character.iast.end} -- {character.telugu.beginning}<b>{character.telugu.sound}</b>{character.telugu.end} -- {character.meaning}</p>
 
                 <div className="overflow-x-auto">
                     <table className="table">
